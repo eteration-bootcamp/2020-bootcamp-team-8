156e42dc3dbebc05aa42d7aabf0a93788e1e42fd
Unknown <ezgi.imamoglu@bilgiedu.net>
2020-08-09 21:05:32 +0300

    first init with navbar implementation

---
+++ eb-frontend/src/components/Compare/Compare.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Compare.module.css';
+
+const Compare = () => {
+  return <h1 className={styles.container}>Compare</h1>;
+};
+
+export default Compare;
